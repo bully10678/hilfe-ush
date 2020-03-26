b32c8280c42e857a693befb6d4b9668a4e412caa
Raul <rwolf2001@gmail.com>
2020-03-26 16:54:41 +0100

    autofill

--- ticket_extended/models/ticket_extension.py
+++ ticket_extended/models/ticket_extension.py
@@ -8,4 +8,12 @@ class HelpdeskTicketExtension(models.Model):
     partner_phone = fields.Char(string='Telefon')
     partner_data_protection = fields.Boolean(string='Datenschutz')
 
+    @api.onchange('partner_id')
+    def _onchange_partner_id_extended(self):
+        if self.partner_id:
+            self.partner_street = self.partner_id.street
+            self.partner_house_number = self.partner_id.x_house_number
+            self.partner_phone = self.partner_id.phone
+            self.partner_data_protection = self.partner_id.x_data_protection
+
 
