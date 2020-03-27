b01c2d480fbb44a1cfd4c4218090bac9f094969a
Raul <rwolf2001@gmail.com>
2020-03-27 15:18:55 +0100

    debugging

--- ticket_extended/models/ticket_extension.py
+++ ticket_extended/models/ticket_extension.py
@@ -30,7 +30,7 @@ class HelpdeskTicketExtension(models.Model):
 
     def handleTeamType(self):
         #information taken out of the db, adjust if needed
-        self.description = self.team_id + ' ' + self.tag_ids
+        self.description = self.team_id
         help_alerts_id = 8
         volunteer_id = 5
         if self.team_id == help_alerts_id:
