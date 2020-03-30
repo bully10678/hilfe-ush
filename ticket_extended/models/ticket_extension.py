c63cf2bdd74a331be031f299ca5f46d53bdc6609
Raul <rwolf2001@gmail.com>
2020-03-30 10:27:53 +0200

    small change

--- ticket_extended/models/ticket_extension.py
+++ ticket_extended/models/ticket_extension.py
@@ -23,8 +23,8 @@ class HelpdeskTicketExtension(models.Model):
         # volunteer_id = 5
         team_obj = self.pool.get('helpdesk.team')
         tag_obj = self.pool.get('helpdesk.tag')
-        _logger.info(team_obj)
-        _logger.info(tag_obj)
+        _logger.warning(team_obj)
+        _logger.warning(tag_obj)
         res_teams = team_obj.search(self,[('display_name', '=like', team_name)])
         res_tags = tag_obj.search(self,[('display_name', '=like', tag_name)])
         result_teams = []
