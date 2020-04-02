159594def8858ddfc966c01cb5c847f3eaa6ec42
Raul <rwolf2001@gmail.com>
2020-04-02 18:03:36 +0200

    Overriden basic form to allow cors

---
+++ ticket_extended/controllers/controllers.py
@@ -0,0 +1,16 @@
+from odoo.addons.website_helpdesk_form.controllers.controllers import WebsiteForm
+from odoo import http
+from odoo.http import request
+
+class customWebsiteForm(WebsiteForm):
+
+    @http.route('''/helpdesk/<model("helpdesk.team", "[('use_website_helpdesk_form','=',True)]"):team>/submit''',
+                type='http', auth="public", website=True, cors='*')
+    def website_helpdesk_form(self, team, **kwargs):
+        if not team.active or not team.website_published:
+            return request.render("website_helpdesk.not_published_any_team")
+        default_values = {}
+        if request.env.user.partner_id != request.env.ref('base.public_partner'):
+            default_values['name'] = request.env.user.partner_id.name
+            default_values['email'] = request.env.user.partner_id.email
+        return request.render("website_helpdesk_form.ticket_submit", {'team': team, 'default_values': default_values})
